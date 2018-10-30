abe084da472a839826ff6b0e42aa5e31a0f018ea
dangerdemon <44249284+dangerdemon@users.noreply.github.com>
2018-10-30 01:42:25 -0500

    update for lab6

--- lab4.ps1
+++ lab4.ps1
@@ -1,15 +1,19 @@
+function employeelist{
 New-Item -Path c:\fileserver -ItemType directory
-
+#employee list file creation
 $list = Import-CSV c:\employeelist.csv
-
 foreach($name in $list){
     $namelower = $name.Name.replace(' ', '').tolower()
     new-item -itemtype directory -path c:\fileserver\ -name $namelower
     new-item -itemtype file -path c:\fileserver\$namelower -name "hello.txt"
 }
-
-
-
-
-
-    
+} 
+Function printers{
+#printer list file creation
+$printerlist = get-printers
+New-Item -Path c:\printerlist -ItemType directory
+foreach($printer in $printerlist){
+    new-item -itemtype directory -path c:\printerlist\ -name $$printer
+    get-itemproperty $printer | convertto-Json | out-file -filepath c:\printerlist\$printer
+}
+}    
